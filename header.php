af5cfd12b658934c3cd076d1eddbbff9efed3566
Adriana Palmero <105382931+APalmeroT@users.noreply.github.com>
2022-06-23 14:59:14 -0500

    Add files via upload
    includes

---
+++ header.php
@@ -0,0 +1,8 @@
+<header class="hero">
+       <div class="container spacing">
+           <img src="http://167.71.185.28/img/logo.png" alt="" class="logo-principal">
+           <!--<h1 class="primary-title" id="Titulo">Be cool, be nice</h1>-->
+           <p>Descubre lo inesperado</p>
+           <a href="municipios.php" class="btn">Explora Tamaulipas</a>
+       </div>
+</header>
---
+++ info-login.php
@@ -0,0 +1,6 @@
+<div class="info-login">
+    <div class="centrar-info">
+        <a href="http://167.71.185.28/login.php">Iniciar Sesión</a>
+        <a href="http://167.71.185.28/registrar.php">Registrarse</a>
+    </div>
+</div>
